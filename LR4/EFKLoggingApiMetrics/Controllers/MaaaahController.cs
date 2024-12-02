b0e00f7dce89208030f553af276baaaf997346a2
Danylo <31706307+JokerFunny@users.noreply.github.com>
2024-12-03 00:35:29 +0200

    - Add LR4 source code.

---
+++ LR4/EFKLoggingApiMetrics/Controllers/MaaaahController.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFKLoggingApiMetrics.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class MaaaahController : ControllerBase
+	{
+		private readonly ILogger<MaaaahController> _logger;
+		public MaaaahController(ILogger<MaaaahController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet("GetRandomMaaaahValue", Name = "GetRandomMaaaahValue")]
+		public int GetRandomMaaaahValue()
+		{
+			_logger.LogInformation("GetRandomMaaaahValue requested.");
+
+			var randomValue = new Random().Next(0, 100);
+			_logger.LogInformation("Maaaaaaaaaaah value is [{RandomMaaaahValue}].", randomValue);
+
+			return randomValue;
+		}
+
+		[HttpGet("ThrowErrorMaaaaaahMessage/{id}", Name = "ThrowErrorMaaaaaahMessage")]
+		public string ThrowErrorMaaaaaahMessage(int id)
+		{
+			_logger.LogInformation("ThrowErrorMaaaaaahMessage requested.");
+
+			try
+			{
+				if (id <= 0)
+				{
+					Response.StatusCode = (int)HttpStatusCode.Forbidden;
+					throw new Exception($"id cannot be less than or equal to 0. Value passed is [{id}].");
+				}
+
+				return id.ToString();
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, ex.Message);
+			}
+
+			return string.Empty;
+		}
+	}
+}
---
+++ LR4/EFKLoggingApiMetrics/Program.cs
@@ -0,0 +1,59 @@
+using Prometheus;
+using Serilog;
+
+namespace EFKLoggingApiMetrics
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Configure Serilog using appsettings.json.
+			Log.Logger = new LoggerConfiguration()
+				.ReadFrom.Configuration(new ConfigurationBuilder()
+					.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+					.Build())
+				.CreateLogger();
+
+			try
+			{
+				Log.Information("Starting the application.");
+
+				var builder = WebApplication.CreateBuilder(args);
+
+				// Add Serilog to the application
+				builder.Host.UseSerilog();
+
+				// Add services to the container.
+				builder.Services.AddControllers();
+				builder.Services.AddEndpointsApiExplorer();
+				builder.Services.AddSwaggerGen();
+				builder.Services.UseHttpClientMetrics();
+
+				var app = builder.Build();
+
+				// Configure the HTTP request pipeline.
+				if (app.Environment.IsDevelopment())
+				{
+					app.UseSwagger();
+					app.UseSwaggerUI();
+				}
+
+				app.UseAuthorization();
+				app.UseMetricServer();
+				app.UseHttpMetrics();
+
+				app.MapControllers();
+
+				app.Run();
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "Application startup failed.");
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+		}
+	}
+}
